8d7a1843e9df6cd442675c5bae237a76b8f66f94
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-12-29 19:45:59 -0500

    add/ loading done

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -3,16 +3,23 @@ import { getGifs } from "../services/getGifs";
 import { Gif } from "./Gif";
 
 const ListOfGifs = ({params}) => {
-  //console.log(props)
   const { keyword } = params;
+  
+  const [loading, setLoading] = useState(false);
   const [gifs, setGifs] = useState([]);
 
   useEffect(
     function(){
+      setLoading(true)
       getGifs( {keyword} )
-      .then(gifs => setGifs(gifs))
+      .then(gifs => {
+        setGifs(gifs)
+        setLoading(false)
+      })
     } ,[keyword]
   )
+
+  if(loading) return <i>Cargando ...</i>
 
   return (
     <>
